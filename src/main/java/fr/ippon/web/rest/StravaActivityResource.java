ccfa3f7b971ce773b6041a972a7b0f66add27206
Quentin Monmert <qmonmert@MacBook-Pro-de-Cedric.local>
2017-05-08 16:34:36 +0200

    Back : get strava activities

--- src/main/java/fr/ippon/web/rest/StravaActivityResource.java
+++ src/main/java/fr/ippon/web/rest/StravaActivityResource.java
@@ -8,11 +8,14 @@ import fr.ippon.web.rest.util.HeaderUtil;
 import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,97 +26,41 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class StravaActivityResource {
 
+    private final static String STRAVA_URL = "https://www.strava.com/api/v3";
+    private final static String ACCESS_TOKEN = "7352ab3baeb484779ced1f9a35c03bcd4340a403";
+
     private final Logger log = LoggerFactory.getLogger(StravaActivityResource.class);
 
-    private static final String ENTITY_NAME = "stravaActivity";
-        
     private final StravaActivityRepository stravaActivityRepository;
 
     public StravaActivityResource(StravaActivityRepository stravaActivityRepository) {
         this.stravaActivityRepository = stravaActivityRepository;
     }
 
-    /**
-     * POST  /strava-activities : Create a new stravaActivity.
-     *
-     * @param stravaActivity the stravaActivity to create
-     * @return the ResponseEntity with status 201 (Created) and with body the new stravaActivity, or with status 400 (Bad Request) if the stravaActivity has already an ID
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @PostMapping("/strava-activities")
-    @Timed
-    public ResponseEntity<StravaActivity> createStravaActivity(@RequestBody StravaActivity stravaActivity) throws URISyntaxException {
-        log.debug("REST request to save StravaActivity : {}", stravaActivity);
-        if (stravaActivity.getId() != null) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "idexists", "A new stravaActivity cannot already have an ID")).body(null);
-        }
-        StravaActivity result = stravaActivityRepository.save(stravaActivity);
-        return ResponseEntity.created(new URI("/api/strava-activities/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
-            .body(result);
-    }
-
-    /**
-     * PUT  /strava-activities : Updates an existing stravaActivity.
-     *
-     * @param stravaActivity the stravaActivity to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated stravaActivity,
-     * or with status 400 (Bad Request) if the stravaActivity is not valid,
-     * or with status 500 (Internal Server Error) if the stravaActivity couldnt be updated
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @PutMapping("/strava-activities")
-    @Timed
-    public ResponseEntity<StravaActivity> updateStravaActivity(@RequestBody StravaActivity stravaActivity) throws URISyntaxException {
-        log.debug("REST request to update StravaActivity : {}", stravaActivity);
-        if (stravaActivity.getId() == null) {
-            return createStravaActivity(stravaActivity);
-        }
-        StravaActivity result = stravaActivityRepository.save(stravaActivity);
-        return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME, stravaActivity.getId().toString()))
-            .body(result);
-    }
-
-    /**
-     * GET  /strava-activities : get all the stravaActivities.
-     *
-     * @return the ResponseEntity with status 200 (OK) and the list of stravaActivities in body
-     */
     @GetMapping("/strava-activities")
     @Timed
     public List<StravaActivity> getAllStravaActivities() {
         log.debug("REST request to get all StravaActivities");
-        List<StravaActivity> stravaActivities = stravaActivityRepository.findAll();
-        return stravaActivities;
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<StravaActivity[]> response = restTemplate.getForEntity(
+            STRAVA_URL + "/athlete/activities?access_token=" + ACCESS_TOKEN + "&page=1&per_page=5",
+            StravaActivity[].class
+        );
+        log.info(response.getBody().toString());
+        return Arrays.asList(response.getBody());
     }
 
-    /**
-     * GET  /strava-activities/:id : get the "id" stravaActivity.
-     *
-     * @param id the id of the stravaActivity to retrieve
-     * @return the ResponseEntity with status 200 (OK) and with body the stravaActivity, or with status 404 (Not Found)
-     */
     @GetMapping("/strava-activities/{id}")
     @Timed
     public ResponseEntity<StravaActivity> getStravaActivity(@PathVariable Long id) {
         log.debug("REST request to get StravaActivity : {}", id);
-        StravaActivity stravaActivity = stravaActivityRepository.findOne(id);
+        RestTemplate restTemplate = new RestTemplate();
+        StravaActivity stravaActivity = restTemplate.getForObject(
+            STRAVA_URL + "/activities/" + id +"?access_token=" + ACCESS_TOKEN,
+            StravaActivity.class
+        );
+        log.info(stravaActivity.toString());
         return ResponseUtil.wrapOrNotFound(Optional.ofNullable(stravaActivity));
-    }
-
-    /**
-     * DELETE  /strava-activities/:id : delete the "id" stravaActivity.
-     *
-     * @param id the id of the stravaActivity to delete
-     * @return the ResponseEntity with status 200 (OK)
-     */
-    @DeleteMapping("/strava-activities/{id}")
-    @Timed
-    public ResponseEntity<Void> deleteStravaActivity(@PathVariable Long id) {
-        log.debug("REST request to delete StravaActivity : {}", id);
-        stravaActivityRepository.delete(id);
-        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString())).build();
     }
 
 }
